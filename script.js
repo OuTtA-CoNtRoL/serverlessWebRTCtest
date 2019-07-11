91a014afc1ea31aaec8dbc7d896767e523338307
Chris <20611246+OuTtA-CoNtRoL@users.noreply.github.com>
2019-07-11 12:34:27 +0200

    Added: hide debug

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ document.addEventListener('DOMContentLoaded',() => {
 	nameSender = document.getElementById('name');
 	nameSender.addEventListener('change', evt => {
 		commObj.name = nameSender.value;
-		addMessage('debug', 'DEBUG', 'Name set to: ' + nameSender.value);
+		addMessage('info', 'INFO', 'Name set to: ' + nameSender.value);
 		requestAnimationFrame(() => nameSender.value='');
 		updateCommObj();
 	});
 	randomName = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	debugCheckBox = document.getElementById('hideDebug');
 	out.innerText += 'Welcome';
 	setupRTC();
 });
@@ -27,6 +28,8 @@ var baseLink;
 var nameSender;
 var nameReceiver = '';
 var randomName;
+var debug;
+var debugCheckBox;
 function setupRTC() {
 	rtc = new RTCPeerConnection({
 		iceServers:[
@@ -72,7 +75,7 @@ function setupRTC() {
 		foreignCommObj = JSON.parse(atob(document.location.hash.slice(1)));
 		applyForeignObj(foreignCommObj);
 		commObj.name = 'Client' + randomName;
-		addMessage('debug', 'DEBUG', 'Name set to: Client' + randomName);
+		addMessage('info', 'INFO', 'Name set to: Client' + randomName);
 		updateCommObj = function() {
 			base.value = btoa(JSON.stringify(commObj));
 			baseLink.value = document.location.origin + document.location.pathname + "#" + btoa(JSON.stringify(commObj));
@@ -99,18 +102,18 @@ function applyForeignObj(foreignObj) {
 
 function setupDataChannel() {
 	dataChannel.onopen = e => {
-		addMessage('debug', 'DEBUG', 'Connection established!');
+		addMessage('info', 'INFO', 'Connection established!');
 		console.log(e);
 		requestAnimationFrame(() => input.value='');
 		nameSender.disabled = true;
 		requestAnimationFrame(() => nameSender.value='');
 	}
 	dataChannel.onclose = e => {
-		addMessage('debug', 'DEBUG', 'Connection closed!');
+		addMessage('info', 'INFO', 'Connection closed!');
 		console.log(e);
 	}
 	dataChannel.onerror = e => {
-		addMessage('debug', 'DEBUG', 'Error!');
+		addMessage('info', 'INFO', 'Error!');
 		console.log(e);
 	}
 	dataChannel.onmessage = e => {
@@ -125,8 +128,12 @@ function setupDataChannel() {
 
 function addMessage(type, name, message){
 	var output = document.getElementById('chat').innerHTML;
+	var setVis = 'display:block';
+	if (debugCheckBox.checked == true && type == 'debug') {
+		setVis = 'display:none';
+	}
 	
-	output += '\n\t\t\t<div class = "' + type + '">';
+	output += '\n\t\t\t<div class = "' + type + '" style = "' + setVis + '">';
 	output += '\n\t\t\t\t<div class = "name">' + name;
 	output += '\n\t\t\t\t\t<div class = "time">' + getTime() + '</div>';
 	output += '\n\t\t\t\t</div>';
@@ -156,25 +163,37 @@ function getDate() {
 function copyLink() {
 	if ((commObj.name == undefined) || (commObj.name == '')) {
 		commObj.name = 'Client' + randomName;
-		addMessage('debug', 'DEBUG', 'Name set to: Client' + randomName);
+		addMessage('info', 'INFO', 'Name set to: Client' + randomName);
 		updateCommObj();
 	}
 	baseLink.select();
 	document.execCommand('copy');
-	addMessage('debug', 'DEBUG', 'Copied Base64-Link to clipboard!');
+	addMessage('info', 'INFO', 'Copied Base64-Link to clipboard!');
 }
 
 function copyBase() {
 	if ((commObj.name == undefined) || (commObj.name == '')) {
 		commObj.name = 'Client' + randomName;
-		addMessage('debug', 'DEBUG', 'Name set to: Client' + randomName);
+		addMessage('info', 'INFO', 'Name set to: Client' + randomName);
 		updateCommObj();
 	}
 	base.select();
 	document.execCommand('copy');
-	addMessage('debug', 'DEBUG', 'Copied Base64-Code to clipboard!');
+	addMessage('info', 'INFO', 'Copied Base64-Code to clipboard!');
 }
 
 function reset() {
 	window.location.href = document.location.origin + document.location.pathname;
+}
+
+function hideDebug() {
+	console.log("pressed");
+	debug = document.getElementsByClassName('debug');
+	for (var i = 0; i < debug.length; i++) {
+		if (debugCheckBox.checked == true) {
+			debug[i].style.display = 'none';
+		} else {
+			debug[i].style.display = 'block';
+		}
+	}
 }
